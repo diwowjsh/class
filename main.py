97fef441ae2d8950bf02e7e85aba2315b48039be
diwowjsh <e01048115289@gmail.com>
2025-07-16 10:39:35 +0900

    Update main.py

--- main.py
+++ main.py
@@ -47,10 +47,10 @@ def compute_brightness(observer_x, observer_y, source_x, source_y, lens_x, lens_
     obs_to_lens = vector(observer_x, observer_y, lens_x, lens_y)
     cos_theta = np.dot(obs_to_src, obs_to_lens) / (np.linalg.norm(obs_to_src)*np.linalg.norm(obs_to_lens) + 1e-9)
 
-    abx, aby = obs_to_src  # 여기가 빠지면 오류 발생
+    abx, aby = obs_to_src
+    ab_len2 = abx*abx + aby*aby  # 여기를 조건문 바깥으로 이동했습니다.
 
     if cos_theta > 0 and np.linalg.norm(obs_to_lens) < np.linalg.norm(obs_to_src):
-        ab_len2 = abx*abx + aby*aby
         apx, apy = lens_x - observer_x, lens_y - observer_y
         dot = apx*abx + apy*aby
         t_param = dot / ab_len2
